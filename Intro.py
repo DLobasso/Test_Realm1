14046196226ad44eb361a8a5ef397bc99d369cc6
DLobasso <90361949+DLobasso@users.noreply.github.com>
2021-09-08 21:17:30 -0400

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,9 @@
+points_obtained = 100
+total_points= 100
+percentage= (points_obtained/total_points)*100
+print(percentage)
+
+demo1 = "I am Dylan Lobasso"
+print(demo1[-1])
+
+print("Have a good weekend")
